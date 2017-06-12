12bc84d0b8b1eda683c323340705c93f927d18b6
Aaron Gee-Clough <aaron@accessviolation.org>
2017-06-12 10:42:22 -0400

    Adding startwith check to Generic hex tcp plugin

--- GenericHexTCP.go
+++ GenericHexTCP.go
@@ -1,6 +1,7 @@
 package Threebits_plugins_public
 
 import (
+    "bytes"
     "encoding/hex"
     "flag"
 	"net"
@@ -14,6 +15,7 @@ import (
 type GenericHexTCP struct{
     sendArg []byte
     receiveArg []byte
+    receiveBeginArg []byte
 }
 
 func (s GenericHexTCP)Handle(socket net.Conn, test structures.Test) (bool, string, error) {
@@ -24,11 +26,18 @@ func (s GenericHexTCP)Handle(socket net.Conn, test structures.Test) (bool, strin
     if err != nil {
         return false, "error reading config", err
     }
-    s.receiveArg, err = hex.DecodeString(test.Args.Generic_TCP.Receive)
-    if err != nil {
-        return false, "error reading config", err
+    if test.Args.Generic_TCP.Receive != "" {
+        s.receiveArg, err = hex.DecodeString(test.Args.Generic_TCP.Receive)
+        if err != nil {
+            return false, "error reading config", err
+        }
     }
-
+    if test.Args.Generic_TCP.ReceiveStart != "" {
+            s.receiveBeginArg, err = hex.DecodeString(test.Args.Generic_TCP.ReceiveStart)
+        if err != nil {
+            return false, "error reading config", err
+        }
+    }
 	_, err = socket.Write(s.sendArg)
 	if err != nil{
 		return false, "error writing to socket", err
@@ -38,14 +47,26 @@ func (s GenericHexTCP)Handle(socket net.Conn, test structures.Test) (bool, strin
 	if err != nil{
 		return false, "error reading response", err
 	}
-	if numBytes != len(s.receiveArg){
-		return false, "response different size than test response string", nil
-	}
-	if reflect.DeepEqual(response, s.receiveArg) {
-	    return true, "", nil
+	if s.receiveArg != nil {
+	    if numBytes != len(s.receiveArg){
+		    return false, "response different size than test response string", nil
+	    }
+	    if reflect.DeepEqual(response, s.receiveArg) {
+	        return true, "", nil
+	    } else {
+	        return false, "handshake did not match", nil
+	    }
 	} else {
-	    return false, "handshake did not match", nil
+	    if numBytes < len(s.receiveBeginArg) {
+	        return false, "response shorter than test comparison string", nil
+	    }
+	    if bytes.HasPrefix(response, s.receiveBeginArg){
+	        return true, "", nil
+	    } else {
+	        return false, "handshake did not match", nil
+	    }
 	}
+	    
 }
 
 
@@ -58,6 +79,7 @@ func (s GenericHexTCP) DefineArguments() {
     
     flag.StringVar(&structures.AllArgs.Generic_TCP.Send, "hexsend", "", "(for generic hex plugin)Hex string to send to target")
     flag.StringVar(&structures.AllArgs.Generic_TCP.Receive, "hexreceive", "", "(for generic hex plugin)Hex string to check responses against.")
+    flag.StringVar(&structures.AllArgs.Generic_TCP.ReceiveStart, "hexreceiveatstart", "", "(for generic hex plugin)Hex string to check just the beginning of the response against.")
 }
 
 
